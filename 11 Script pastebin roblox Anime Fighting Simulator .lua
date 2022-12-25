141a0d45a87f5762859deae2bec4690b0bdcf239
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 16:58:31 +0100

    commit zero

---
+++ 11 Script pastebin roblox Anime Fighting Simulator .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/2dgeneralspam1/scripts-and-stuff/master/scripts/garfield%20hub", true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							It has a built-in admin gui, chat spam, anty-afk, Esp, setwalkspeed and many other features!
+						
+]]
